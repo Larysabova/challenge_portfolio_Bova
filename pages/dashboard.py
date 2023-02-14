21d9e2b667594eb5841c3e433c1a44ec916ca8e6
Larysabova <larysabova@gmail.com>
2023-02-14 21:08:00 +0100

    Update dashboard.py

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -9,10 +9,10 @@ class Dashboard(BasePage):
     main_page_button_xpath = "//*[text()='Main page']"
     main_page_button_in_Polish_xpath = "//*[text()='Strona główna']"
     players_button_xpath = "//*[text()='Players']"
-    language_button_xpath = "//*[text()='English' or text()='Polski']"
+    language_button_xpath = "//ul[2]/div[1]"
     sign_out_button_xpath = "//*[text()='Sign out']"
     dev_team_contact_button_xpath = "//*[1][name() = 'a']"
-    add_player_button_xpath = "//span[contains(text(),'Add pl')]"
+    add_player_button_xpath = "//*[2][name() = 'a']"
     last_created_player_hyperlink_xpath = "//*[3][name() = 'a']/button/span[1]"
     last_updated_player_hyperlink_xpath = "//a[2]/button/span[1]"
     last_created_match_hyperlink_xpath = "//a[3]/button/span[1]"
@@ -23,11 +23,11 @@ class Dashboard(BasePage):
     wait = WebDriverWait(driver, 10)
 
     def title_of_page(self):
-        self.wait_for_element_to_be_clicable(self.add_player_button_xpath)
+        self.wait_for_element_to_be_clickable(self.add_player_button_xpath)
         assert self.get_page_title(self.dashboard_url) == self.expected_title
 
-    def wait_for_button_will_be_clicable(self):
-        self.wait_for_element_to_be_clicable(self.sign_out_button_xpath)
+    def wait_for_button_will_be_clickable(self):
+        self.wait_for_element_to_be_clickable(self.sign_out_button_xpath)
 
     def click_on_the_add_player_button(self):
         self.click_on_the_element(self.add_player_button_xpath)
