76bca22222fc71ed7269cac2832dd28bfbb63d3a
Larysabova <larysabova@gmail.com>
2023-02-14 21:08:08 +0100

    Update matches_page.py

--- pages/matches_page.py
+++ pages/matches_page.py
@@ -5,20 +5,20 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 
 class Matches(BasePage):
-    add_match_button = "//main/a/button/span[1]"
+    add_match_button_xpath = "//main/a/button/span[1]"
     matches_url = ('https://scouts-test.futbolkolektyw.pl/en')
     expected_title = "Matches player Bora Lava"
-    added_match_popup = "//*[text() = 'Added match.']"
+    added_match_popup_xpath = "//*[text() = 'Added match.']"
 
     def title_of_page(self):
-        self.wait_for_element_to_be_clicable(self.add_match_button)
+        self.wait_for_element_to_be_clickable(self.add_match_button_xpath)
         assert self.get_page_title(self.matches_url) == self.expected_title
 
     def click_on_the_add_match_button(self):
-        self.click_on_the_element(self.add_match_button)
+        self.click_on_the_element(self.add_match_button_xpath)
 
     def wait_for_visibility_of_popup(self):
-        self.wait_for_visibility_of_element_located(self.added_match_popup)
+        self.wait_for_visibility_of_element_located(self.added_match_popup_xpath)
 
-    def wait_for_button_will_be_clicable(self):
-        self.wait_for_element_to_be_clicable(self.add_match_button)
+    def wait_for_button_will_be_clickable(self):
+        self.wait_for_element_to_be_clickable(self.add_match_button_xpath)
