8cfd784882d19aba17754c1d45de5c31ca4aee06
Larysabova <larysabova@gmail.com>
2023-02-14 21:07:51 +0100

    Update add_match_form.py

--- pages/add_match_form.py
+++ pages/add_match_form.py
@@ -30,7 +30,7 @@ class AddAMatchForm(BasePage):
     expected_title = "Adding match player Bora Lava"
 
     def title_of_page(self):
-        self.wait_for_element_to_be_clicable(self.submit_button_xpath)
+        self.wait_for_element_to_be_clickable(self.submit_button_xpath)
         assert self.get_page_title(self.add_match_url) == self.expected_title
 
     def type_in_my_team(self, my_team):
@@ -51,5 +51,5 @@ class AddAMatchForm(BasePage):
     def click_on_the_submit_button(self):
         self.click_on_the_element(self.submit_button_xpath)
 
-    def wait_for_button_will_be_clicable(self):
-        self.wait_for_element_to_be_clicable(self.submit_button_xpath)
+    def wait_for_button_will_be_clickable(self):
+        self.wait_for_element_to_be_clickable(self.submit_button_xpath)
